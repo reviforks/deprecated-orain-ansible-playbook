3642d07058349cc201d5492e6bdc6250c32c2f2d
Kudu <kudu.py@gmail.com>
2014-04-05 18:37:12 -0400

    Make the AWS keys purpose-specific

--- roles/common/templates/backup.sh
+++ roles/common/templates/backup.sh
@@ -1,5 +1,5 @@
-export AWS_ACCESS_KEY_ID="{{AWS_ACCESS_KEY_ID}}"
-export AWS_SECRET_ACCESS_KEY="{{AWS_SECRET_ACCESS_KEY}}"
+export AWS_ACCESS_KEY_ID="{{AWS_BACKUPS_ACCESS_KEY_ID}}"
+export AWS_SECRET_ACCESS_KEY="{{AWS_BACKUPS_SECRET_ACCESS_KEY}}"
 export PASSPHRASE=""
 duplicity --full-if-older-than 30D --encrypt-key={{duplicity_key_id}} --exclude=/tmp --exclude=/var/tmp --exclude=/proc --gpg-options="-z6" / {{duplicity_path}}
 export AWS_ACCESS_KEY_ID=""
