0dfdd00bd4ea35d53251541ede74c437b29a9aaf
John Lewis <johnflewis93@gmail.com>
2014-08-04 17:15:56 +0000

    Revert "load new RC formatter and use it"
    This reverts commit 14872a0864815342ef5d0a7ec992cd5c0af158b0.

--- roles/mediawiki/files/localsettings/LocalExtensions.php
+++ roles/mediawiki/files/localsettings/LocalExtensions.php
@@ -195,7 +195,7 @@ if ( $wmgUseEchoPack ) {
 
 if ( $wmgUseIRCRC ) {
 	$wgRCFeeds['Orain'] = array(
-		'formatter' => 'OrainRCFeedFormatter',
+		'formatter' => 'IRCColourfulRCFeedFormatter',
 		'uri' => 'udp://prod4.orain.org:5070',
 		'add_interwiki_prefix' => false,
 		'omit_bots' => true,
