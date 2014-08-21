78593bb17c74a88739ecc1928d1e4307ca437f07
Kudu <kudu.py@gmail.com>
2014-08-21 12:22:30 -0400

    Clean up get_db_list.py

--- roles/mediawiki/files/get_db_list.py
+++ roles/mediawiki/files/get_db_list.py
@@ -1,18 +1,24 @@
-import requests
+import requests, sys
 
 def main():
+    # Don't support Python 2.x
+    if sys.version < '3':
+        return
+
+    # Download the database list
     r = requests.get("https://meta.orain.org/w/index.php?title=Database_list&action=raw")
 
+    # Return if the download wasn't successful (e.g. site is broken)
     if r.status_code != 200:
         return
 
-    text = r.text.replace('<pre>\n', '').replace('</pre>', '')
+    # Remove the <pre> formatting tags and trailing newline
+    text = r.text.replace('<pre>\n', '').replace('</pre>', '').rstrip('\n')
 
-    if text[-1:] == "\n":
-        text = text[:-1]
-
-    with open("/usr/share/nginx/.orain.org/w/all.dblist", "w", encoding="utf8") as db_list:
+    # Write to a file to be read by our settings
+    with open("/usr/share/nginx/.orain.org/w/all.dblist", "w") as db_list:
         db_list.write(text)
 
 if __name__ == "__main__":
     main()
+
