fcaf3b3db1e4eb1bcad6842fa7642f05bdf48724
John Lewis <johnflewis93@gmail.com>
2014-08-25 22:29:01 +0000

    use a multi-master set up and add allthetropeswiki to c2

---
+++ roles/mediawiki/files/localsettings/Database.php
@@ -0,0 +1,36 @@
+<?php
+
+$wgLBFactoryConf = array(
+	'class' => 'LBFactoryMulti',
+	'sectionsByDB' => array(
+		'allthetropeswiki' => 'c2',
+		'centralauth' => 'c1', // Not covered by default
+		'globalblocking' => 'c1', // Not covered by default
+	),
+	'sectionLoads' => array(
+		'DEFAULT' => array( // ALL wikis unless overriden
+			'prod3' => 1,
+		),
+		'c1' => array( // Non-wikis needing to be directed to c1
+			'prod3' => 1,
+		),
+		'c2' => array(
+			'prod5' => 1,
+		),
+	),
+	'serverTemplate' => array(
+		'dbname' => $wgDBname,
+		'user' => $wgDBuser,
+		'password' => $wgDBpassword,
+		'type' => 'mysql',
+	),
+	'hostsByName' => array(
+		'prod3' => '10.131.243.49',
+		'prod5' => '10.131.243.243',
+	),
+	// Below is required to be here. This also allows us to easily take a cluster out of service for a reason
+	'readOnlyBySection' => array(
+	//	'DEFAULT' => '',
+	//	'c2' => '',
+	),
+);
