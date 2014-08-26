300eb5c2c036c2a7b3aa96a75f25518a40c7eeff
John Lewis <johnflewis93@gmail.com>
2014-08-26 01:47:45 +0000

    mailman configs

---
+++ roles/mail/files/mm_cfg.py
@@ -0,0 +1,17 @@
+from Defaults import *
+
+MAILMAN_SITE_LIST = 'mailman'
+DEFAULT_URL_PATTERN = 'http://%s/'
+IMAGE_LOGOS = '/images/mailman/'
+
+DEFAULT_EMAIL_HOST = 'lists.orain.org'
+
+DEFAULT_URL_HOST = 'lists.orain.org'
+add_virtualhost(DEFAULT_URL_HOST, DEFAULT_EMAIL_HOST)
+
+DEFAULT_SERVER_LANGUAGE = 'en'
+
+USE_ENVELOPE_SENDER = 0
+DEFAULT_SEND_REMINDERS = 0
+
+MTA=Postfix
