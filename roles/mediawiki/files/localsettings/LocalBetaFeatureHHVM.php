38342278bfe926ff179be5e642e19354099a730a
John Lewis <johnflewis93@gmail.com>
2014-09-20 23:13:31 +0000

    Add a HHVM tag to RC for analysis of usage

--- roles/mediawiki/files/localsettings/LocalBetaFeatureHHVM.php
+++ roles/mediawiki/files/localsettings/LocalBetaFeatureHHVM.php
@@ -27,9 +27,17 @@ $wgHooks['BeforePageDisplay'][] = function ( OutputPage $out ) {
 		$req->response()->setcookie( 'hhvm', '0', -1, array( 'prefix' => '' ) );
 	}
 };
+$wgHooks['RecentChange_save'][] = function ( RecentChange $rc ) {
+	if ( wfIsHHVM() ) {
+		ChangeTags::addTags( 'HHVM', $rc->getAttribute( 'rc_id' ) );
+	}
+	return true;
+};
 $wgHooks['LocalisationCacheRecache'][] = function ( $cache, $code, &$cachedData ) {
 	if ( $code === 'en' ) {
 		$cachedData['messages']['hhvm-beta-label'] = 'HHVM';
 		$cachedData['messages']['hhvm-beta-desc'] = 'HHVM is a new and faster PHP runtime.';
+		$cachedData['messages']['tag-HHVM'] = '[[m:Forum:HHVM|HHVM]]';
+		$cachedData['messages']['tag-HHVM-description'] = 'Changes made using HHVM. These are tagged for analysis purposes';
 	}
 };
