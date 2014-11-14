94580f542674579ae2ef240b145ef236a6403264
John F. Lewis <johnflewis93@gmail.com>
2014-11-14 23:43:19 +0000

    add udp bot script

---
+++ roles/irc/files/udp.py
@@ -0,0 +1,42 @@
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+from twisted.words.protocols import irc
+from twisted.internet import protocol
+import sys
+ 
+recver = None
+ 
+class RCBot(irc.IRCClient):
+    nickname = "Orain"
+    channel = "#Orain-feed"
+    def signedOn(self):
+        global recver
+        self.join(self.channel)
+        print "Signed on as %s." % (self.nickname,)
+        recver = self
+ 
+    def joined(self, channel):
+        print "Joined %s." % (channel,)
+ 
+    def gotUDP(self, broadcast):
+        self.msg(self.channel, broadcast)
+ 
+class RCFactory(protocol.ClientFactory):
+    protocol = RCBot
+ 
+    def clientConnectionLost(self, connector, reason):
+        print "Lost connection (%s), reconnecting." % (reason,)
+        connector.connect()
+ 
+    def clientConnectionFailed(self, connector, reason):
+        print "Could not connect: %s" % (reason,)
+ 
+class Echo(DatagramProtocol):
+ 
+    def datagramReceived(self, data, (host, port)):
+        global recver
+        recver.gotUDP(data)
+
+reactor.listenUDP(5070, Echo())
+reactor.connectTCP("hwan.irc.jasperswebsite.com", 6667, RCFactory())
+reactor.run()
