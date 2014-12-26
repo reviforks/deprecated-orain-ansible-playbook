f81d2bd5fd5e44c52bcf8673535b87f1f3625ba3
John F. Lewis <johnflewis93@gmail.com>
2014-12-26 22:30:43 +0000

    remove backup.sh

--- roles/base/templates/backup.sh
+++
@@ -1,7 +0,0 @@
-export AWS_ACCESS_KEY_ID="{{AWS_BACKUPS_ACCESS_KEY_ID}}"
-export AWS_SECRET_ACCESS_KEY="{{AWS_BACKUPS_SECRET_ACCESS_KEY}}"
-export PASSPHRASE=""
-duplicity --full-if-older-than 30D --encrypt-key={{duplicity_key_id}} --exclude=/tmp --exclude=/var/tmp --exclude=/proc --gpg-options="-z6" / {{duplicity_path}}
-export AWS_ACCESS_KEY_ID=""
-export AWS_SECRET_ACCESS_KEY=""
-
