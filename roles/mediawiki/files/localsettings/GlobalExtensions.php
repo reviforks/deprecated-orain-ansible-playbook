988a90d621827ec9a23833dcf7e66418d93c3904
Arcane21 <Arcane21@users.noreply.github.com>
2015-01-17 21:19:58 -0600

    removed Nimbus, causing issues

--- roles/mediawiki/files/localsettings/GlobalExtensions.php
+++ roles/mediawiki/files/localsettings/GlobalExtensions.php
@@ -51,4 +51,3 @@ require_once( "$IP/skins/Vector/Vector.php" );
 require_once( "$IP/skins/MonoBook/MonoBook.php" );
 require_once( "$IP/skins/CologneBlue/CologneBlue.php" );
 require_once( "$IP/skins/Modern/Modern.php" );
-require_once( "$IP/skins/Nimbus/Nimbus.php" );
