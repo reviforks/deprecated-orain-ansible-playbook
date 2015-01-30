8e8673f6ffc0d3ed45236b365906a7d8753e0f22
Andrew "FastLizard4" Adams <FastLizard4-Git-Commits@fastlizard4.org>
2015-01-29 20:03:39 -0800

    Re-enable showing critical PHP errors to debuggers
    Enables showing critical PHP errors (E_ALL & ~E_NOTICE &~E_STRICT) to users with
    the debug global user right.

--- roles/mediawiki/files/localsettings/LocalDebugUsers.php
+++ roles/mediawiki/files/localsettings/LocalDebugUsers.php
@@ -9,7 +9,8 @@ function ifUserCanDebug() {
 	global $wgUser, $wgShowExceptionDetails, $wgDebugToolbar, $wgDevelopmentWarnings, $wgShowSQLErrors, $wgShowDBErrorBacktrace;
 	if ( $wgUser->isAllowed( 'debug' ) ) {
 		//error_reporting( -1 ); // phpredis introduces a *lot* of array issues due to live hack within MediaWiki
-		//ini_set( 'display_errors', 1 ); // above
+		error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT); //Here, try this.  Should limit the displayed errors to only the important ones.
+		ini_set( 'display_errors', 1 );
 		$wgShowExceptionDetails = true;
 		$wgDebugToolbar = true;
 		$wgDevelopmentWarnings = true;
