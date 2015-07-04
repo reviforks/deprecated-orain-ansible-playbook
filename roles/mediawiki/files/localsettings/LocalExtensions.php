020ee0000403d2ed573692af1f5403050a5f5e0e
addshore <addshorewiki@gmail.com>
2015-07-04 10:03:28 +0200

    Add missing break for encyclopediumwiki
    Before this time this wiki would have had
    ALLLLLL extensions loaded.
    
    *slaps* the person that missed this when adding
    this......

--- roles/mediawiki/files/localsettings/LocalExtensions.php
+++ roles/mediawiki/files/localsettings/LocalExtensions.php
@@ -110,6 +110,7 @@ switch( $wgDBname ) {
 		   'descriptionCacheExpiry'  => 43200, // 12 hours, optional (values are seconds)
 		   'apiThumbCacheExpiry'     => 43200, // 12 hours, optional, but required for local thumb caching
 		);
+		break;
 	case 'extloadwiki':
 		// all extensions are loaded on this wiki
 		require_once "$IP/extensions/AdManager/AdManager.php";
